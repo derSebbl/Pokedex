0361f4478d648cdbbbd1966baf0fa549df2b0de0
Sebastian Binz <sebastianbinz@AirvonSebastian.fritz.box>
2023-09-28 21:00:47 +0200

    all new - good way

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-let AllPokemonsUrl = [];
 let AllPokemons = [];
-let PokeInfo = [];
+let load = 20;
+let currentLoad = 0;
 
-
-async function init(){
-    await AddPokemon();
-    await loadPokedex();
-    PushInfo();
-}
-
+function loadMore() {
+    load += 20;
+    loadPokemon();
+  }
 
 async function AddPokemon(){
     let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
     let response = await fetch(url);
     let responseAsJson = await response.json();
-    let results = responseAsJson['results'];
+    AllPokemons = responseAsJson['results'];
+    loadPokemon();
+}
 
-    for (i = 0; i < results.length; i++){
-        const pokemon = results[i]['name'];
-        const url = results[i]['url']
-        AllPokemons.push(pokemon);
-        AllPokemonsUrl.push(url);
+async function loadPokemon(){
+    for (let i = currentLoad; i < load; i++) {
+        if (i >= AllPokemons.length) {
+        return;}
+        currentLoad++;
+        let PokemonUrl = AllPokemons[i].url;
+        let Pokemon = await fetch(PokemonUrl);
+        let currentPokemon = await Pokemon.json();
+        renderPokedex(currentPokemon, i);
     }
 }
 
-function loadPokedex(){
-    for (let i = 0; i < AllPokemons.length; i++) {
-        const pokemon = AllPokemons[i];
-        let container = document.getElementById('pokedex');
-
-        container.innerHTML +=`<div id="pokemon${i}">
-        <h3>${pokemon}</h3>
-        </div>`;
-    }
-}
-
-async function PushInfo(){
-    for (let i = 0; i < AllPokemonsUrl.length; i++) {
-        const url = AllPokemonsUrl[i];
-        let response = await fetch(url);
-        let responseAsJson1 = await response.json();
-        PokeInfo.push(responseAsJson1);
-    }
-}
-
-function loadPictures (){
-    let picture = PokeInfo[0]['id'];
-    console.log(picture);
+function renderPokedex(currentPokemon, i){
+    let container = document.getElementById('pokedex');
+    let card = document.createElement("div");
+    card.className = "card";
+    card.setAttribute = ("data-url", currentPokemon.url); // damit die URL gespeichert wird um die richtigen Daten zu laden
+    card.addEventListener = ('click', function() {detailcard(currentPokemon, i)});// DOSN'T WORK?!?
+    card.innerHTML = `<div class="cardHead">
+    <div class="pokeName">${currentPokemon.name}</div>
+    <div class="pokeId">${"#" + currentPokemon.id}</div>
+    </div>
+    <img src="${currentPokemon.sprites.other["official-artwork"].front_default}">`;
+    container.appendChild(card);
 }
