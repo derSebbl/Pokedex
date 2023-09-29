fa4b2b340ce0a3989fc7aaa81342cb8f3ddc3e99
Sebastian Binz <sebastianbinz@AirvonSebastian.fritz.box>
2023-09-29 21:51:30 +0200

    Detailcard Beta

--- script.js
+++ script.js
@@ -32,11 +32,39 @@ function renderPokedex(currentPokemon, i){
     let card = document.createElement("div");
     card.className = "card";
     card.setAttribute = ("data-url", currentPokemon.url); // damit die URL gespeichert wird um die richtigen Daten zu laden
-    card.addEventListener = ('click', function() {detailcard(currentPokemon, i)});// DOSN'T WORK?!?
+    card.onclick = function(){detail(currentPokemon, i)};// 
     card.innerHTML = `<div class="cardHead">
     <div class="pokeName">${currentPokemon.name}</div>
     <div class="pokeId">${"#" + currentPokemon.id}</div>
     </div>
     <img src="${currentPokemon.sprites.other["official-artwork"].front_default}">`;
     container.appendChild(card);
+}
+
+function detail(currentPokemon, i){
+    removeDNone();
+    let container = document.getElementById('pokeDetail');
+    container.innerHTML += /*html*/`
+    <div class="DetailCard">
+            <div><img src="${currentPokemon.sprites.other["official-artwork"].front_default}"></div>
+            <div>${currentPokemon.name}</div>
+            <div class="DetailType">type: ${currentPokemon.types["0"].type.name}</div>
+        <div class="DetailStats">
+            <div>${currentPokemon.stats["0"].stat.name}: ${currentPokemon.stats["0"].base_stat}</div>
+            <div>${currentPokemon.stats["1"].stat.name}: ${currentPokemon.stats["1"].base_stat}</div>
+            <div>${currentPokemon.stats["2"].stat.name}: ${currentPokemon.stats["2"].base_stat}</div>
+            <div>${currentPokemon.stats["5"].stat.name}: ${currentPokemon.stats["5"].base_stat}</div>
+        </div>
+        <button onclick="addDNone()">X</button>
+    </div>`;
+}
+
+function removeDNone(){
+    document.getElementById('pokeDetail').classList.remove('d-none');
+}
+
+function addDNone(){
+    document.getElementById('pokeDetail').classList.add('d-none'); 
+    let container = document.getElementById('pokeDetail');
+    container.innerHTML =``;
 }
